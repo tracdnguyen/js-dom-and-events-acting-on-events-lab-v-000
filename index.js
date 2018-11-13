c213e0f5518dcb150638ab9d38deb42a2acde4cb
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-13 07:24:23 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation(){
+  return input.value
+}
+
+function addNewElementAsLi(){
+  let employeeInfo = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeInfo}</li>`)
+}
+
+function resetInput(){
+  document.querySelector('input').value = ''
+}
+
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+    resetInput()
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  let a = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  a.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  })
+}
